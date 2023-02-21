707a67706e8cd139a2e46ef5b689a632d50ec1ac
Rafael Carrasco Cisneros <rcarrascocisne9122@sdsu.edu>
2023-02-20 19:48:53 -0800

    added different functions for smaller data manipulation

--- word_definitions-webapi.py
+++ word_definitions-webapi.py
@@ -1,10 +1,35 @@
 import requests
+import json
 
 # Word
-my_word = "apple"
+my_word = "watch"
 
 # Get Info
 req = requests.get(f"https://api.dictionaryapi.dev/api/v2/entries/en/{my_word}")
 
-# Print result
+# Print full requested data for the word
 print(req.text)
+
+# Convert to JSON
+data = req.json()
+
+# Access the first item in the list, since it only contains one item
+word_data = data[0]
+
+# Access the "meanings" list
+meanings = word_data["meanings"]
+
+# Access the first item in the "meanings" list
+first_meaning = meanings[0]
+
+# Access the "definitions" list within the first meaning
+definitions = first_meaning["definitions"]
+
+# Access the first item in the "definitions" list
+first_definition = definitions[1]
+
+# Access the "synonyms" list within the first definition
+synonyms = first_definition["synonyms"]
+
+# Print the list of synonyms
+print(synonyms)
--- word_definitions_localapi.py
+++ word_definitions_localapi.py
@@ -1,16 +1,80 @@
+import os
 import json
 
-file_path = "./data/DA.json"
+# Creates a list of dictionary files
+def load_data_list(directory_path):
+    data = []
+    # Loop through the files in the directory
+    for filename in os.listdir(directory_path):
+        if filename.endswith(".json"):
+            # Build the full path to the file
+            file_path = os.path.join(directory_path, filename)
+            # Open the file
+            with open(file_path, "r") as file:
+                # Load the JSON data from file
+                data.append(json.load(file))
 
-# Open the file
-with open(file_path, "r") as file:
-    # Load the JSON data from file
-    data = json.load(file)
+    return data
 
-# Access elements in the JSON data
-word = data["ABATE"]
-synonyms = word["SYNONYMS"]
-synonym1 = synonyms[1]
-print(synonym1)
-# print(data["ABATE"])
-print(data["ABATE"]["SYNONYMS"])
+def load_data_dict(directory_path):
+    data = {}
+    # Loop through the files in the directory
+    for filename in os.listdir(directory_path):
+        if filename.endswith(".json"):
+            # Build the full path to the file
+            file_path = os.path.join(directory_path, filename)
+            # Open the file
+            with open(file_path, "r") as file:
+                # Load the JSON data from file
+                json_data = json.load(file)
+                # Update the data dictionary with the loaded data
+                data.update(json_data)
+
+    return data
+
+
+# Finds a word in a list of dictionaries and returns its info
+def find_key(data, key):
+    for dictionary in data:
+        if key in dictionary:
+            return dictionary[key]
+            # return dictionary
+
+# Extracts a specific property of a word
+def get_property(word_info, property):
+    word_property = word_info[property]
+    return word_property
+
+# Extracts the synonyms from the word info
+def get_synonyms(word_info):
+    synonyms = word_info["SYNONYMS"]
+    return synonyms
+
+def convert_to_dict(data):
+    # Convert list to dictionary
+    data_dict = {}
+    for item in data:
+        data_dict.update(item)
+
+# Creates a list of a specific property of all words
+def get_property_list(data, property):
+    property_list = []
+    for key in data.keys():
+        meanings = data[key].get("MEANINGS", {})
+        synonyms = meanings.get(property, [])
+        property_list.extend(synonyms)
+    return property_list
+
+# Set the path to the directory containing the JSON files
+directory_path = "semantic-primes/data"
+
+# Load Data
+data_list = load_data_list(directory_path)
+data_dict = load_data_dict(directory_path)
+
+# Choose Word
+key = "SEE"
+
+word_info = find_key(data_list, key)
+word_synonyms = get_synonyms(word_info)
+print(word_synonyms)
